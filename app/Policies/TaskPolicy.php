7aa38a585c585423fdff1a9a2b7533abbe9a871d
suiso01 <nsk_729104@gmail.com>
2024-01-29 15:26:29 +0900

    first commit

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Task;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TaskPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+       * 指定されたユーザーのタスクのとき削除可能
+       *
+       * @param User $user
+       * @param Task $task
+       * @return bool
+       */
+   public function destroy(User $user, Task $task)
+   {
+       return $user->id === $task->user_id;
+   }
+}
